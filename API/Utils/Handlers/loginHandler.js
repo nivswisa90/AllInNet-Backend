68fdb2df442d26d5ca911241c4ccef0688efc412
martinmazas <mazasmartin@gmail.com>
2022-04-24 16:23:48 +0300

    adding user register

--- API/Utils/Handlers/loginHandler.js
+++ API/Utils/Handlers/loginHandler.js
@@ -1,5 +1,5 @@
 const Users = require("../../../DB/Schemas/users")
-const { logger } = require("../logger");
+const {logger} = require("../logger");
 const moment = require("moment");
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
@@ -8,32 +8,34 @@ const privateKey = process.env.PRIVATE_KEY;
 
 exports.loginHandler = {
     login(req, res) {
-        Users.findOne({ email: req.body.values.email })
+        Users.findOne({email: req.body.values.email})
             .then(docs => {
                 if (docs) {
                     if (!bcrypt.compareSync(req.body.values.password, docs.password)) {
                         res.json('Wrong user or password');
-                    }
-                    else {
+                    } else {
                         const id = docs._id;
-                        const token = jwt.sign({ id }, privateKey);
-                        // res.cookie('token', token, { maxAge: 6000000, sameSite: 'none', secure: true });
-                        res.json({ msg: "Successfully connected", token: token }).status(200);
+                        const currentUser = {
+                            token: jwt.sign({id}, privateKey),
+                            name: docs.firstName,
+                            role: docs.role
+                        }
+                        res.json({msg: "Successfully connected", user: currentUser}).status(200);
                     }
-                }
-                else {
+                } else {
                     res.json('User does not exist').status(400);
                 }
             })
             .catch(err => console.log(`Error getting the data from DB: ${err}`));
     },
     addUser(req, res) {
+        console.log(req.body)
         const newUser = new Users({
-            "firstName": req.body.firstName,
-            "lastName": req.body.lastName,
-            "email": req.body.email,
-            "password": bcrypt.hashSync(req.body.password, 10),
-            "role": req.body.role
+            "firstName": req.body.values.firstName,
+            "lastName": req.body.values.lastName,
+            "email": req.body.values.email,
+            "password": bcrypt.hashSync(req.body.values.password, 10),
+            "role": req.body.values.role
         })
         newUser.save()
             .then(docs => res.send(docs).status(200))
