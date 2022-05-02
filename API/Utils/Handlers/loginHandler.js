5126e469355e9c06117c7ebce76c1c868580b96a
nivswisa90 <nivswisa9@gmail.com>
2022-05-02 22:00:56 +0300

    return role for the user

--- API/Utils/Handlers/loginHandler.js
+++ API/Utils/Handlers/loginHandler.js
@@ -91,7 +91,8 @@ exports.loginHandler = {
                     level: "info",
                     message: `Successfully get all team player`,
                 })
-                const user = {name: docs[0].firstName, id: docs[0].id}
+                console.log('docs',docs[0])
+                const user = {name: docs[0].firstName, id: docs[0].id, role:docs[0].role}
                 res.send(user).status(200)
             }).catch(err => console.log(err))
     }
